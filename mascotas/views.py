e1508ed8c3297cd13362a4c9991d7a46fda306ec
santiagomijaress.18 <edward.gov.co@outlook.es>
2026-03-08 12:09:19 -0500

    Filtros en catálogo de masctoas

--- mascotas/views.py
+++ mascotas/views.py
@@ -4,8 +4,20 @@ from .models import Mascota
 
 def catalogo(request):
     mascotas = Mascota.objects.filter(estado='disponible')
+
+    # Filtros
+    especie = request.GET.get('especie')
+    sexo    = request.GET.get('sexo')
+
+    if especie:
+        mascotas = mascotas.filter(especie=especie)
+    if sexo:
+        mascotas = mascotas.filter(sexo=sexo)
+
     return render(request, 'mascotas/catalogo.html', {
-        'mascotas': mascotas
+        'mascotas': mascotas,
+        'especie_activa': especie or '',
+        'sexo_activo': sexo or '',
     })
 
 
